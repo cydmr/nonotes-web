c9fc73f6455ca001b97a124a412f61ffc1fa1e9c
ceydademirbolu@gmail.com <ceydademirbolu@gmail.com>
2020-07-13 14:24:23 +0300

    host

--- client/src/helpers/request.js
+++ client/src/helpers/request.js
@@ -2,12 +2,13 @@ import {message} from 'antd';
 import Cookies from 'universal-cookie';
 const cookies = new Cookies();
 const host = 'http://localhost:4000';
+const host_heroku = 'https://mysterious-headland-65781.herokuapp.com/';
 
 export const request = {
   post: async (url, data, secured) => {
     if (secured && !cookies.get('token')) return false;
     try {
-      const res = await fetch(host + url, {
+      const res = await fetch(host_heroku + url, {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors',
         headers: {
@@ -30,7 +31,7 @@ export const request = {
   get: async url => {
     if (!cookies.get('token')) return false;
     try {
-      const res = await fetch(host + url, {
+      const res = await fetch(host_heroku + url, {
         method: 'GET', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors',
         headers: {
@@ -51,7 +52,7 @@ export const request = {
   put: async (url, data) => {
     if (!cookies.get('token')) return false;
     try {
-      const res = await fetch(host + url, {
+      const res = await fetch(host_heroku + url, {
         method: 'PUT', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors',
         headers: {
@@ -74,7 +75,7 @@ export const request = {
   delete: async url => {
     if (!cookies.get('token')) return false;
     try {
-      const res = await fetch(host + url, {
+      const res = await fetch(host_heroku + url, {
         method: 'DELETE', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors',
         headers: {
